b0e6da8a99234a9839ad450ffdaedf6fd289b95a
tsprograms <tsgithub@gmail.com>
2016-04-09 16:10:59 -0400

    Add tokenize() from interpret.js

--- src.js
+++ src.js
@@ -11,7 +11,6 @@ Copyright © 2016 TSPrograms.
   var in = function(msg) {
     return window.prompt(msg);
   };
-  
   var Context = function() {
     var undefined;
     var RESERVED = ['=', ':', '?', '?..', '>>', '<<', '+', '++', 'true', 'false', 'nil'];
@@ -106,5 +105,38 @@ Copyright © 2016 TSPrograms.
     
     this.call = call;
     this._vars = variables;
+  };
+  var context = new Context();
+  var tokenize = function(codeString) {
+    codeString += ''; // Make sure codeString is a string.
+    var levels = 0;
+    var tokenized = [''];
+    var index = 0;
+    var containsParts = codeString.charAt(0) === '(' || (/\s/).test(codeString);
+    if (!containsParts) {
+      return codeString;
+    }
+    for (var i = 0; i < codeString.length; ++i) {
+      if (levels === 0 && (/\s/).test(codeString.charAt(i))) {
+        ++index;
+        tokenized[index] = '';
+      }
+      else if (tokenized[index] === '' && codeString.charAt(i) === '(') {
+        ++levels;
+      }
+      else if (codeString.charAt(i) === ')' && (/\s|$/).test(codeString.charAt(i + 1))) {
+        --levels;
+      }
+      else {
+        tokenized[index] += codeString.charAt(i);
+      }
+    }
+    var retokenized = [];
+    for (var i = 0; i < tokenized.length; ++i) {
+      if (tokenized[i] !== '') {
+        retokenized.push(tokenize(tokenized[i]));
+      }
+    }
+    return retokenized;
   };
 })();
