63575f771bc025df552a8d265e659d0ae62e9274
tsprograms <tsgithub@gmail.com>
2016-04-09 11:14:02 -0400

    Add basic tokenize function to interpret.js

--- interpret.js
+++ interpret.js
@@ -7,6 +7,34 @@ Copyright © 2016 TSPrograms.
 (function() {
   var tokenize = function(codeString) {
     codeString += ''; // Make sure codeString is a string.
-    // TODO
+    var levels = 0;
+    var tokenized = [''];
+    var index = 0;
+    var containsParts = codeString.charAt(0) === '(' || (/\s/).test(codeString.charAt(i));
+    if (!containsParts) {
+      return codeString;
+    }
+    for (var i = 0; i < codeString.length; ++i) {
+      if (levels === 0 && (/\s/).test(codeString.charAt(i))) {
+        ++index;
+        tokenized[index] = '';
+      }
+      else if (tokenized[index] === '' && codeString.charAt(i) === '(') {
+        ++levels;
+      }
+      else if (codeString.charAt(i) === ')' && (/\s/).test(codeString.charAt(i + 1))) {
+        --levels;
+      }
+      else {
+        tokenized[index] += codeString.charAt(i);
+      }
+    }
+    var retokenized = [];
+    for (var i = 0; i < tokenized.length; i++) {
+      if (tokenized[i] !== '') {
+        retokenized.push(tokenize(tokenized[i]));
+      }
+    }
+    return retokenized;
   };
 })();
