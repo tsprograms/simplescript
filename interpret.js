ea505e0427596e2dbec183cc5d39830d8df069b0
tsprograms <tsgithub@gmail.com>
2016-04-09 10:42:46 -0400

    Update interpret.js

--- interpret.js
+++ interpret.js
@@ -2,3 +2,11 @@
 This file created on 4/9/16 by TSPrograms.
 Copyright © 2016 TSPrograms.
 */
+
+// Enclose code in an anonymous function to prevent implicit globals.
+(function() {
+  var tokenize = function(codeString) {
+    codeString += ''; // Make sure codeString is a string.
+    // TODO
+  };
+})();
